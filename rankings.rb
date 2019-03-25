eeb2dca4a5502560f2c240095797c9c95b450a66
Juanse Gimenez <juansegimenez94@gmail.com>
2019-03-25 19:41:20 -0300

    Added input file,teams and matches method to generate structures of data

--- rankings.rb
+++ rankings.rb
@@ -1,8 +1,27 @@
 class FileParser
 	attr_accessor :filepath, :filecontent
 
+	REGEXP = {
+    name: /^\w+\s?\w+[:digits:]/,
+    score: /\d+/,
+    name_and_score: /(\w+\s?\w+)\s(\d)/
+  }
+  
 	def initialize(name)
     @filepath = name
+  end
+
+  def teams
+    parse_file.lines.each_with_object([]) do |line, matches|
+      teams = line.strip.split(',')
+      teams.each {|team| matches << team.scan(REGEXP[:name_and_score])}
+    end.map{|v| v[0][0] }.uniq
+  end
+
+  def matches
+    parse_file.lines.each_with_object([]) do |line, matches|
+      matches << line.scan(REGEXP[:name_and_score])
+    end
   end
 
   private
