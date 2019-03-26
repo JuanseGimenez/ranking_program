74eb1870648b184aac721990d73f21bb7e365fc1
Juanse Gimenez <juanse@sinequix.com>
2019-03-25 21:37:03 -0300

    Formatted with rubocop

--- rankings.rb
+++ rankings.rb
@@ -12,8 +12,8 @@ class League
   attr_accessor :teams, :matches
 
   def initialize(teams = [], matches = [])
-    @teams   = teams.map { |team| Team.new(team) } 
-    @matches = matches.map { |match| add_match_results(match)}
+    @teams   = teams.map { |team| Team.new(team) }
+    @matches = matches.map { |match| add_match_results(match) }
   end
 
   # Aggregate score to team it depends to result of match
@@ -23,9 +23,9 @@ class League
   # Max_by: Used to returns the object in enum that gives the maximum value from the given block.
   # https://apidock.com/ruby/Enumerable/max_by
   def add_match_results(match)
-  	if match[0][1] == match[1][1] 
-      team1 = @teams.find {|team| team.name == match[0][0]}
-      team2 = @teams.find {|team| team.name == match[1][0]}
+    if match[0][1] == match[1][1]
+      team1 = @teams.find { |team| team.name == match[0][0] }
+      team2 = @teams.find { |team| team.name == match[1][0] }
 
       team1.score += 1
       team2.score += 1
@@ -33,10 +33,10 @@ class League
       winner = match.max_by { |x| x[1] }
       losser = match.min_by { |x| x[1] }
 
-      team1 = @teams.find {|team| team.name == winner[0]}
-      team2 = @teams.find {|team| team.name == losser[0]}
+      team1 = @teams.find { |team| team.name == winner[0] }
+      team2 = @teams.find { |team| team.name == losser[0] }
 
-      team1.score += 3 
+      team1.score += 3
     end
     team1.goals += match[0][1].to_i
     team2.goals += match[1][1].to_i
@@ -46,13 +46,13 @@ class League
   # First: return the first element of the enumerable
   # https://apidock.com/ruby/Enumerable/first
   def winner
-  	scoreboard.first
+    scoreboard.first
   end
 
   # Should return scoreboard sorted by score and sorted by name if two teams have the same score
-	# Method Sort: used to sort result by score descending and by name ascending if score values are equals => https://apidock.com/ruby/Enumerable/sort
+  # Method Sort: used to sort result by score descending and by name ascending if score values are equals => https://apidock.com/ruby/Enumerable/sort
   def scoreboard
-  	teams.sort { |team1, team2| team1.score == team2.score ? team1.name <=> team2.name : team2.score <=> team1.score }.each_with_object([]) do |team, scoreboard|
+    teams.sort { |team1, team2| team1.score == team2.score ? team1.name <=> team2.name : team2.score <=> team1.score }.each_with_object([]) do |team, scoreboard|
       scoreboard << { name: team.name, score: team.score, goals: team.goals }
     end
   end
@@ -62,22 +62,22 @@ class Team
   attr_accessor :name, :score, :goals
 
   def initialize(name)
-  	@name  = name
-  	@score = 0
-  	@goals = 0
+    @name = name
+    @score = 0
+    @goals = 0
   end
 end
 
 class FileParser
-	attr_accessor :filepath, :filecontent
+  attr_accessor :filepath, :filecontent
 
-	REGEXP = {
+  REGEXP = {
     name: /^\w+\s?\w+[:digits:]/,
     score: /\d+/,
     name_and_score: /(\w+\s?\w+)\s(\d)/
-  }
+  }.freeze
 
-	def initialize(name)
+  def initialize(name)
     @filepath = name
   end
 
@@ -88,8 +88,8 @@ class FileParser
   def teams
     parse_file.lines.each_with_object([]) do |line, matches|
       teams = line.strip.split(',')
-      teams.each {|team| matches << team.scan(REGEXP[:name_and_score])}
-    end.map{|v| v[0][0] }.uniq
+      teams.each { |team| matches << team.scan(REGEXP[:name_and_score]) }
+    end.map { |v| v[0][0] }.uniq
   end
 
   # Should return matches list
@@ -108,13 +108,14 @@ class FileParser
 
   # Should Export scoreboard in output.txt file
   def export_board(board)
-    file = File.open("output.txt", "w")
+    file = File.open('output.txt', 'w')
     board.each_with_index do |team, index|
       file.puts "#{index + 1}. #{team[:name]}, #{team[:score]} pts"
     end
   end
 
   private
+
   # Should be return content of file
   def parse_file
     @filecontent ||= File.read(@filepath)
@@ -127,18 +128,18 @@ file.show_board(league.scoreboard)
 file.export_board(league.scoreboard)
 
 test = TestSuite.new
-test.expect(league.winner[:name], "Lions")
-test.expect(league.scoreboard, [{:name=>"Lions", :score=>7, :goals=>9},
-																{:name=>"Tarantulas", :score=>7, :goals=>5},
-																{:name=>"FC Awesome", :score=>4, :goals=>4},
-																{:name=>"Snakes", :score=>3, :goals=>10},
-																{:name=>"Grouches", :score=>1, :goals=>3}])
-test.expect(file.teams, ["Lions", "Snakes", "Tarantulas", "FC Awesome", "Grouches"])
-test.expect(file.matches, [[["Lions", "4"], ["Snakes", "3"]],
-													 [["Tarantulas", "1"], ["FC Awesome", "0"]],
-													 [["Lions", "1"], ["FC Awesome", "1"]],
-													 [["Tarantulas", "3"], ["Snakes", "1"]],
-													 [["Lions", "4"], ["Grouches", "0"]],
-													 [["Tarantulas", "1"], ["Grouches", "1"]],
-													 [["Snakes", "4"], ["Grouches", "2"]],
-													 [["FC Awesome", "3"], ["Snakes", "2"]]])
+test.expect(league.winner[:name], 'Lions')
+test.expect(league.scoreboard, [{ name: 'Lions', score: 7, goals: 9 },
+                                { name: 'Tarantulas', score: 7, goals: 5 },
+                                { name: 'FC Awesome', score: 4, goals: 4 },
+                                { name: 'Snakes', score: 3, goals: 10 },
+                                { name: 'Grouches', score: 1, goals: 3 }])
+test.expect(file.teams, ['Lions', 'Snakes', 'Tarantulas', 'FC Awesome', 'Grouches'])
+test.expect(file.matches, [[%w[Lions 4], %w[Snakes 3]],
+                           [%w[Tarantulas 1], ['FC Awesome', '0']],
+                           [%w[Lions 1], ['FC Awesome', '1']],
+                           [%w[Tarantulas 3], %w[Snakes 1]],
+                           [%w[Lions 4], %w[Grouches 0]],
+                           [%w[Tarantulas 1], %w[Grouches 1]],
+                           [%w[Snakes 4], %w[Grouches 2]],
+                           [['FC Awesome', '3'], %w[Snakes 2]]])
